8358fa16431049e18693f0de8104ade9eeba36c4
ojanjiwa <149864968+CrossFode@users.noreply.github.com>
2024-03-30 09:12:19 +0700

    Program.cs

--- modul6_1302220004/Program.cs
+++ modul6_1302220004/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// See https://aka.ms/new-console-template for more information
 using System.Diagnostics.Contracts;
 using System.Security.Cryptography.X509Certificates;
 
